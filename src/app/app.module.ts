5a05cee4196ae2835a6c6ba38d40926419ceb8d8
peter rader <raderp841@gmail.com>
2022-07-30 14:09:39 -0400

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { PostComponent } from './post/post.component';
+import { SubpostComponent } from './subpost/subpost.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    UserComponent,
+    PostComponent,
+    SubpostComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/subpost/subpost.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subpost',
+  templateUrl: './subpost.component.html',
+  styleUrls: ['./subpost.component.css']
+})
+export class SubpostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
